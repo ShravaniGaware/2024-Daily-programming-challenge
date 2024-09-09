8ddcb2dd81c13045ec8038283c3ea8509ed368fa
ShravaniGaware <132206967+ShravaniGaware@users.noreply.github.com>
2024-09-09 22:58:15 +0530

    Createday1.java

---
+++ day1.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class array_sorting {
+    public static void main(String[] args){
+        int arr[]={0,1,2,1,0,2,1,0};
+        Arrays.sort(arr);
+        System.out.println("the sorted array is"+Arrays.toString(arr));
+    }
+}
