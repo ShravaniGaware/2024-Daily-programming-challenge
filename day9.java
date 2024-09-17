1a04ae764417f47c2b931f41f3dbd50da8bdb6c5
ShravaniGaware <132206967+ShravaniGaware@users.noreply.github.com>
2024-09-17 22:15:16 +0530

    Update day9.java

--- day9.java
+++ day9.java
@@ -1,26 +1,22 @@
-import java.util.Arrays;
-public class day10{
-    public static String longestcommonprefix(String[] strs){
-     int i=0;
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        
         Arrays.sort(strs);
-        String str1=strs[0];
-       String str2=strs[strs.length-1];
-       for( i=1;i<strs.length;i++){
-        if(str1.charAt(i)==str2.charAt(i)){
-             i++;
+        
+        String str1 = strs[0];
+        String str2 = strs[strs.length - 1];
+        int i = 0;
+        
+    
+        while (i < str1.length() && i < str2.length()) {
+            if (str1.charAt(i) == str2.charAt(i)) {
+                i++;
+            } else {
+                break;
+            }
         }
-        else{
-            break;
-        }
-       
-        return i == 0 ? "" : str1.substring(0, i);
-     }
-        return str1.substring(0,i);
+        
+        return str1.substring(0, i);
     }
-    
-    
-    public static void main(String[] args){
-     String strs[]={"flower","flow","flight"};
-     System.out.println(longestcommonprefix(strs));
-}
 }
